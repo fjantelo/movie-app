0e57978845e233374070c93a8009b40e2f0462ef
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-06 19:41:54 -0400

    Adds restful routes and controller methods, replaces all previous

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
-  get "/actor/:id", controller: "actors", action: "actor"
+  # get "/actor/:id", controller: "actors", action: "actor"
+  # get "/movies" => "movies#index"
+  # get "/movies/:id" => "movies#show"
+  # get "/actor" => "actors#query"
+  # get "/actorid" => "actors#bodyparams"
+
   get "/movies" => "movies#index"
   get "/movies/:id" => "movies#show"
-  get "/actor" => "actors#query"
-  get "/actorid" => "actors#bodyparams"
+  post "/movies" => "movies#create"
+  patch "/movies/:id" => "movies#update"
+  delete "/movies/:id" => "movies#destroy"
+
+  get "/actors" => "actors#index"
+  get "/actors/:id" => "actors#show"
+  post "/actors" => "actors#create"
+  patch "/actors/:id" => "actors#update"
+  delete "/actors/:id" => "actors#destroy"
 end
