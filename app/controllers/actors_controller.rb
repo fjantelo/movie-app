0267bc8dec124c5c7d3ec32ac2e1be984dd5574e
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-05 22:18:30 -0400

    Adds query route and controller method

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,9 @@ class ActorsController < ApplicationController
     actor = Actor.find_by(id: params["id"])
     render json: actor.as_json
   end
+
+  def query
+    actor = Actor.find_by(id: params["q"])
+    render json: actor.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/actor/:id", controller: "actors", action: "actor"
   get "/movies" => "movies#index"
   get "/movies/:id" => "movies#show"
+  get "/actor" => "actors#query"
 end
