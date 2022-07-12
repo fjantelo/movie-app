23af5dbc8a8f097d316935fb05fed9e6f0f82b96
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-11 21:02:17 -0400

    Adds happy and sad paths to movies and actors controllers

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :year, numericality: true
+  validates :plot, length: { in: 6..500 }
+  validates :director, length: { minimum: 2 }
 end
