ca46da59ff4c699c00b4e7c27a6ab915cfad49b3
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-15 18:43:23 -0400

    Adds user model, users controller, and create method and route for users

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
   post "/actors" => "actors#create"
   patch "/actors/:id" => "actors#update"
   delete "/actors/:id" => "actors#destroy"
+
+  post "/users" => "users#create"
 end
