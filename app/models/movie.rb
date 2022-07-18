7a099b07cb0defd2568f410c0f77f213ce42f0f8
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-18 19:14:29 -0400

    Adds associations between Movie, Genre, and MovieGenre models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
   validates :title, presence: true
   validates :year, numericality: true
   validates :plot, length: { in: 6..500 }
